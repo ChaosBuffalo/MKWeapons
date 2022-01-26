e497ad60072a7d6fbf8aecbc6c3ab532a2cbe193
Kovak <kovac1066@gmail.com>
2022-01-25 23:37:10 -0800

    handle spell trigger renames

--- src/main/java/com/chaosbuffalo/mkweapons/items/effects/ranged/IRangedWeaponEffect.java
+++ src/main/java/com/chaosbuffalo/mkweapons/items/effects/ranged/IRangedWeaponEffect.java
@@ -3,7 +3,6 @@ package com.chaosbuffalo.mkweapons.items.effects.ranged;
 import com.chaosbuffalo.mkcore.core.IMKEntityData;
 import com.chaosbuffalo.mkweapons.items.effects.IItemEffect;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.entity.projectile.AbstractArrowEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
@@ -12,7 +11,7 @@ import net.minecraftforge.event.entity.living.LivingHurtEvent;
 public interface IRangedWeaponEffect extends IItemEffect {
 
     default void onProjectileHit(LivingHurtEvent event, DamageSource source, LivingEntity livingTarget,
-                                 ServerPlayerEntity playerSource, IMKEntityData sourceData,
+                                 LivingEntity livingSource, IMKEntityData sourceData,
                                  AbstractArrowEntity arrow, ItemStack bow){
 
     }
